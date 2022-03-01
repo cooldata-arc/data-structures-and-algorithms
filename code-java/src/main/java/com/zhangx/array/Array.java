fedd9d6e184d404cc9a8196e31a2a9b46e04cace
xing1.zhang <xing1.zhang@zhaopin.com.cn>
2022-03-01 17:12:30 +0800

    U

--- code-java/src/main/java/com/zhangx/array/Array.java
+++ code-java/src/main/java/com/zhangx/array/Array.java
@@ -1,12 +1,16 @@
 package com.zhangx.array;
 
+import org.openjdk.jol.info.ClassLayout;
+
 public class Array {
     public static void main(String[] args) {
-        String[] arr = {"google", "baidu", "bing"};
-    
-        System.out.println(arr);
-    
+        // String[] arr = {"google", "baidu", "bing"};    
         // System.out.println(ClassLayout.parseInstance(arr).toPrintable());
+        // System.out.println(ClassLayout.parseInstance(arr[0]).toPrintable());
 
+        // String s = "google";
+        int s = 5;
+        System.out.println(s);
+        // System.out.println(ClassLayout.parseInstance(s).toPrintable());
     }
 }
